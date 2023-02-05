fda3896e20140de99eb3aed85d92c7919dcaf65a
InfiAB <tmtg360@gmail.com>
2023-02-06 02:08:43 +0530

    footer made 🕛

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <div class="text-black text-center p-4 text-xs">
+            © 2023 Copyright: Aditya Bansal
+        </div>
+    );
+}
+
+export default Footer; 
