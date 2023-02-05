3aace2192500284ddc529dca6c32ce12bb7e8f29
InfiAB <tmtg360@gmail.com>
2023-02-05 16:26:44 +0530

    made nav for large screens

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,36 @@
 
 function Header() {
     return (
-        <div className="container mx-auto px-6 py-2 flex justify-between items-center">
-            <div className="hidden lg:block">
-                <ul className="inline-flex">
-                    <li><a className="px-4 font-bold" href="/">Home</a></li>
-                    <li><a className="px-4 hover:text-gray-800" href="#">About</a></li>
-                    <li><a className="px-4 hover:text-gray-800" href="#">Contact</a></li>
-                </ul>
+        <div className="container mx-auto py-4">
+            <div className="md:hidden"> 
+                hei
+            </div>
+
+            {/* for large screens */}
+            <div className="hidden md:block">
+                <div className="flex justify-center">
+                    <ul className="inline-flex">
+                        <li className="px-1">
+                            <button className="px-4 bg-tranparent border border-transparent hover:border-black rounded-full transition duration-500 ease-in-out" href="/">
+                                home
+                            </button>
+                        </li>
+                        <li className="px-1">
+                            <button className="px-4 bg-tranparent border border-transparent hover:border-black rounded-full transition duration-500 ease-in-out" href="#">
+                                work
+                            </button>
+                        </li>
+                        <li className="px-1">
+                            <button className="px-4 bg-tranparent border border-transparent hover:border-black rounded-full transition duration-500 ease-in-out" href="#">
+                                words
+                            </button>
+                        </li>
+{/*                         
+                        <li><a className="px-5 hover:text-green-500" href="/">home</a></li>
+                        <li><a className="px-5 hover:text-gray-800" href="#">work</a></li>
+                        <li><a className="px-5 hover:text-gray-800" href="#">words</a></li> */}
+                    </ul>
+                </div>
             </div>
       </div>
     );
