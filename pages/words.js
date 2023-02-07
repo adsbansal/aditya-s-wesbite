20ed8d410c709c9146052c411b0817087fab1fee
InfiAB <tmtg360@gmail.com>
2023-02-07 13:16:09 +0530

    fixed the linking

--- pages/words.js
+++ pages/words.js
@@ -1,7 +1,29 @@
-function MyApp({ Component, pageProps }) {
+import PostCard from "../components/PostCard";
+import getPosts from "../helpers/getPosts";
+
+export default function Words({ posts }) {
   return (
-    <div>Does this work?</div>
+    <div>
+      <h1 className="mt-1 mb-5 font-semibold text-4xl underline">blogs and learnings.</h1>
+      {posts.map((post) => (
+        <PostCard
+          key={post.slug}
+          title={post.data.title}
+          date={post.data.date}
+          description={post.data.description}
+          slug={post.slug}
+        />
+      ))}
+    </div>
   );
 }
 
-export default MyApp;
+export const getStaticProps = () => {
+  const posts = getPosts();
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
