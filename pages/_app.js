5423a371889c60afd7c2998d0b7c276ba0e231ea
InfiAB <tmtg360@gmail.com>
2023-02-20 03:00:32 +0530

    seo done; projects added 🚀

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,27 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 import Head from "next/head";
+import {DefaultSeo} from 'next-seo';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <DefaultSeo
+          title="Adi's Corner"
+          description="This is Aditya Bansal's portfolio website aka. his space on the internet. This contains more information on him,
+          a list of his projects and a some blogs he's penned down. Website developed by Aditya."
+          openGraph={{
+              type: 'website',
+              locale: 'en_IE',
+              url: 'https://www.url.ie/',
+              siteName: 'SiteName',
+          }}
+          twitter={{
+              handle: '@adsbansal',
+              site: 'adsbansal.org',
+              cardType: 'summary',
+          }}
+      />
       <Head>
         <title> Adi's Corner </title>
         <link rel="icon" type="image/x-icon" sizes="48x48" href="images/favicon2.ico" />
--- pages/work.js
+++ pages/work.js
@@ -5,19 +5,30 @@ export default function Work() {
       <div className="text-slate-300 sm:px-16 sm:mx-16">
         <FadeIn transitionDuration={600} delay={80}>
           <div className="py-4 my-4">
-              <h1 className="mt-6 mb-5 font-semibold text-teal-200 text-3xl underline">Currently:</h1>
+              <h1 className="mt-6 mb-5 font-semibold text-teal-200 text-3xl underline">Currently</h1>
               <p>Learning about blockchains and studying courses including NLP and Advanced Data Science.</p>        
           </div>
           {/* <h1 className="mt-6 mb-5 text-slate-300 font-semibold text-xl underline">Previously:</h1>
           <p></p> */}
 
           <div className="py-4 my-4">
-              <h1 className="mt-6 mb-5 font-semibold text-teal-200 text-3xl underline">Projects I've worked on:</h1>
+              <h1 className="mt-6 mb-5 font-semibold text-teal-200 text-3xl underline">Projects I've worked on</h1>
               <div className="px-1 mx-1">
                   <h3 className="font text-base my-4">
                       → 
-                      <a href="https://github.com/adsbansal/buildspace-dao-starter]" className="ml-2 underline hover:text-teal-200">Decentralized Autonomous Organization App</a>
+                      <a href="https://github.com/adsbansal/buildspace-dao-starter" className="ml-2 underline hover:text-teal-200" target="_blank" rel="noopener noreferrer">Decentralized Autonomous Organization App</a>
                       : Learned the basics of blockchain application and tinkered with web3.js
+                  </h3> 
+                  <h3 className="font text-base my-4">
+                      → 
+                      <a href="https://github.com/adsbansal/FetalHealthClassificaiton" className="ml-2 underline hover:text-teal-200" target="_blank" rel="noopener noreferrer">Fetal Health Classification System</a>
+                      : Developed an machine learning classification project for Fetal Health Classification using multiple models.
+                  </h3> 
+                  <h3 className="font text-base my-4">
+                      → 
+                      <a href="https://drive.google.com/file/d/17I5NAYTWR4uHz6YarFoFSHRz-oo8_y6I/view?usp=sharing" className="ml-2 underline hover:text-teal-200" target="_blank" rel="noopener noreferrer">Ideation Phase: Graphical-Alphabetical Password Authentication System </a>
+                      : Designed a authentication system to increase security by increasing password length whilst not making it difficult for the
+                      user to remember the password.
                   </h3> 
               </div>
           </div>
