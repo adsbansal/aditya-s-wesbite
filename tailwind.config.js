74c0277fde6df16bcc0ab1ee98910eb9f95160ef
InfiAB <tmtg360@gmail.com>
2023-02-20 13:12:55 +0530

    tried something for edge issue 🙏

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,4 +31,18 @@ module.exports = {
 
   },
   plugins: [require("@tailwindcss/typography")],
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|jpg|png)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=9999999999, must-revalidate',
+          }
+        ],
+      },
+    ]
+  },
 };
